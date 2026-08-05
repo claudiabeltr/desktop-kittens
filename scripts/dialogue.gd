1311af3091b13b3198df320d528267b302b46369
Alejandro Piñas <a.pinas@upm.es>
2026-08-05 19:57:37 +0200

    fix: set font size and dialogue position

--- scripts/dialogue.gd
+++ scripts/dialogue.gd
@@ -1,23 +1,22 @@
 extends Node2D
 
-@export var velocidad: float = 20.0  # píxeles por segundo
-
-var limite_superior: float = 141.0
-var limite_inferior: float = 162.0
-var direccion: int = 1
+@export var speed: float = 20.0  # pixels per second
+var upper_limit: float = 141.0
+var lower_limit: float = 162.0
+var direction: int = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# The node smoothly moves up and down, between upper_limit and lower_limit,
+# the child nodes (TextBox and DialogueText) move accordingly
 func _process(delta: float) -> void:
-	position.y += direccion * velocidad * delta
+	position.y += direction * speed * delta
 	
-	if position.y >= limite_inferior:
-		position.y = limite_inferior
-		direccion = -1
-	elif position.y <= limite_superior:
-		position.y = limite_superior
-		direccion = 1
+	if position.y >= lower_limit:
+		position.y = lower_limit
+		direction = -1
+	elif position.y <= upper_limit:
+		position.y = upper_limit
+		direction = 1
