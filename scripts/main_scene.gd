c43f2902e66d741f84f819a1f421aaa11a865b21
claudia <claudiabeltran.apr@gmail.com>
2026-08-07 13:13:54 +0200

    feat: right arrow added

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -7,6 +7,8 @@ extends Node2D
 @onready var dialogue_text: Label = $Dialogue/DialogueText
 @onready var dialogue_textbox: Sprite2D = $Dialogue/TextBox
 @onready var menu: Control = $CatMenu
+@onready var menu2: Control = $NameMenu
+@onready var rightarrow: Control = $RightArrowButton
 
 #Custom enumerate for state machine states
 enum State { IDLE, PETTING, SLEEP }
@@ -129,6 +131,8 @@ func _ready() -> void:
 	
 	# Menu hidden by default
 	menu.visible = false
+	menu2.visible = false
+	rightarrow.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -347,5 +351,13 @@ func _on_exit_button_pressed() -> void:
 func _on_menu_button_toggled(toggled_on: bool) -> void:
 	if(toggled_on == true):
 		menu.visible = true
+		rightarrow.visible = true
 	else:
 		menu.visible = false
+		rightarrow.visible = false
+
+
+func _on_right_arrow_button_pressed() -> void:
+	menu.visible = false
+	menu2.visible = true
+	rightarrow.visible = false
